0ed0cb11624ed3c57ca13770cd257761fa070aa0
Will Cooley <william.cooley@gmail.coom>
2023-07-29 00:08:35 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record){
+    let teamWinSearch = record.find(function(element){
+        return element.result === "W";
+    });
+    if(teamWinSearch) {
+        return teamWinSearch.year;
+    } else {
+        return undefined
+    }
+}
